e9350206e7a63258e26beec11a3c67a91016d327
Valentine Reese <vreese177@gmail.com>
2022-03-11 11:33:14 +0800

    fix computer

--- cal_rem_addliq.py
+++ cal_rem_addliq.py
@@ -4,14 +4,16 @@ from utils import time_util
 class User:
     def __init__(self):
         self.sender = 0
-        self.stake_amount = 0
+        self.token_x = 0
+        self.token_y = 0
         self.remedy_amount = 0
 
 
-def convert_list_string_to_amount(s) -> int:
+def convert_list_string_to_amount(s) -> (int, int):
     s = s.strip("(")
     s = s.strip(")")
-    return int(s.split(",")[1])
+    a = s.split(",")
+    return int(a[0]), int(a[1])
 
 
 def computer_users(opts, multiplier, apy) -> dict:
@@ -22,16 +24,22 @@ def computer_users(opts, multiplier, apy) -> dict:
         if opt.get('opt') != "add_liquidity":
             continue
 
-        stake_amount = convert_list_string_to_amount(opt.get('amount'))
+        token_x, token_y = convert_list_string_to_amount(opt.get('amount'))
+        token_x = token_x / 1e9
+        token_y = token_y / 1e9
+
         if sender not in users:
             user = User()
             user.sender = sender
-            user.stake_amount = stake_amount
+            user.token_x = token_x
+            user.token_y = token_y
         else:
             user = users.get(sender)
-            user.stake_amount = user.stake_amount + stake_amount
-
-        user.remedy_amount = user.stake_amount * multiplier * apy * 16_200 / 31_536_000
+            user.token_x = user.token_x + token_x
+            user.token_y = user.token_x + token_y
         users[sender] = user
+
+    for k, v in users.items():
+        v.remedy_amount = v.token_y * multiplier * apy * 16_200 / 31_536_000
 
     return users
